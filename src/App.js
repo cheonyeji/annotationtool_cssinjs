b767e5c5e91374c33cffcd584dd3abe096e0561f
cheonyeji <ssaltuck@gmail.com>
2021-04-26 08:16:33 +0900

    [add] 로그인 기능 추가해봣는디 수정해야함 프록시가 모임

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyles from "./components/GlobalStyles";
 import LandingPage from "./components/LandingPage";
@@ -8,21 +8,33 @@ import Testing from "./components/Testing";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import { initialState, reducer } from "./components/store";
+
+export const AuthContext = createContext();
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
-      <Router>
-        <div>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/work" component={WorkingPage} />
-            <Route exact path="/test" component={Testing} />
-            <Route exact path="/login" component={LoginPage} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+      <AuthContext.Provider
+        value={{
+          state,
+          dispatch,
+        }}
+      >
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/work" component={WorkingPage} />
+              <Route exact path="/test" component={Testing} />
+              <Route exact path="/login" component={LoginPage} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </AuthContext.Provider>
       <GlobalStyles />
     </>
   );
